495022e38c200fd039c545d8c6c40ea3bf0a8b40
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-01-22 18:49:48 +0100

    First commit

---
+++ src/main/java/RefactoringAndMultiThreading/Main.java
@@ -0,0 +1,13 @@
+package RefactoringAndMultiThreading;
+
+import java.io.*;
+
+
+public class Main {
+    final static int portNumber = 9999;
+    public static void main(String[] args) throws IOException {
+        new Server(portNumber).connectServer();
+
+    }
+}
+
---
+++ src/main/java/RefactoringAndMultiThreading/Server.java
@@ -0,0 +1,32 @@
+package RefactoringAndMultiThreading;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private final int portNumber;
+    public Server(int portNumber) {
+        this.portNumber = portNumber;
+    }
+    public void connectServer() throws IOException {
+        final var serverSocket = new ServerSocket(portNumber);
+        final var threadPool = Executors.newFixedThreadPool(64);
+        System.out.println("The server is STARTING!");
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                final var serverThread = new ServerThread(socket);
+                threadPool.submit(serverThread);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
+
